9981d9035bba782bccddb0e83174e3e50c213a39
sebasnn86 <s.narbay@physaro.com>
2023-04-21 11:55:44 +0200

    Kind of refactored The functions and types can be sent to other file easily it seems. Will keep them toghether for the time being as this is meant to be a test only.

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,25 +1,29 @@
 import { z } from "zod";
 import { procedure, router } from "../trpc";
 
+const addNumbersInput = z.object({ numbers: z.array(z.number()) });
+
+const addNumbers = procedure.input(addNumbersInput).query(({ input }) => ({
+	result: input.numbers.reduce((pv, cv) => pv + cv, 0),
+}));
+
+const mutateSomethingInput = z.object({
+	user: z.string(),
+	text: z.string(),
+});
+
+const mutateSomething = procedure
+	.input(mutateSomethingInput)
+	.mutation(({ input }) => {
+		console.info(`${input.user} -> ${input.text}`);
+		return {
+			ok: 1,
+		};
+	});
+
 export const appRouter = router({
-	addNumbers: procedure
-		.input(z.object({ numbers: z.array(z.number()) }))
-		.query(({ input }) => ({
-			result: input.numbers.reduce((pv, cv) => pv + cv, 0),
-		})),
-	mutateSomething: procedure
-		.input(
-			z.object({
-				user: z.string(),
-				text: z.string(),
-			})
-		)
-		.mutation(({ input }) => {
-			console.info(`${input.user} -> ${input.text}`);
-			return {
-				ok: 1,
-			};
-		}),
+	addNumbers,
+	mutateSomething,
 });
 
 // export type definition of API
